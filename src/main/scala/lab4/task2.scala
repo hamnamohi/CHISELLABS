bc345bf077e052bab40b834ca49f7f6779ef933d
Hamna Mohiuddin <110234585+hamnamohi@users.noreply.github.com>
2022-09-08 23:13:24 +0500

    Update task2.scala

--- src/main/scala/lab4/task2.scala
+++ src/main/scala/lab4/task2.scala
@@ -21,31 +21,31 @@
 //     val io = IO (new LM_IO_Interface_ImmdValGen )
 // // Start coding here
 //     io.immd_se := 0. U
-//     val temp = 0.U
+//    
 //     switch(io.instr(6,0)){
 //         is(i_type){
             
-//             temp :=Fill(20,io.instr(31))
+//             val temp :=Fill(20,io.instr(31))
 //             io.immd_se := Cat(temp, io.instr(31,20))
 //         }
 //         is(s_type){
-//             temp :=Fill(20,io.instr(31))
+//             val temp :=Fill(20,io.instr(31))
 //             io.immd_se := Cat(temp,io.instr(31,25),io.instr(11,7))
 //         }
 //         is(u_type){
-//             temp :=Fill(20,io.instr(31))
+//             val temp :=Fill(20,io.instr(31))
 //             io.immd_se := Cat(temp,io.instr(31,12))
 //         }
 //         is(b_type){
-//             temp :=Fill(20,io.instr(31))
+//             val temp :=Fill(20,io.instr(31))
 //             io.immd_se := Cat(temp,io.instr(31),io.instr(7),io.instr(30,25),io.instr(11,8))
 //         }
 //         is(j_type){
-//             temp :=Fill(20,io.instr(31))
+//             val temp :=Fill(20,io.instr(31))
 //             io.immd_se := Cat(temp,io.instr(31),io.instr(19,12),io.instr(20),io.instr(30,21))
 //         }
 //         is(ld_ins){
-//             temp :=Fill(20,io.instr(31))
+//             val temp :=Fill(20,io.instr(31))
 //             io.immd_se := Cat(temp, io.instr(31,20))
 //         }
 //     }
