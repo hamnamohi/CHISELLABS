821e90b9e07056317ca83bd730e5d589f5493bdd
Hamna Mohiuddin <110234585+hamnamohi@users.noreply.github.com>
2022-09-09 16:44:11 +0500

    Add files via upload

--- src/test/scala/lab4/task2test.scala
+++ src/test/scala/lab4/task2test.scala
@@ -1,40 +1,34 @@
-// package lab4
-// import chisel3._
-// import chisel3.util._
-// import org.scalatest._
-// import chiseltest._
-// import chiseltest.experimental.TestOptionBuilder . _
-// import chiseltest.internal.VerilatorBackendAnnotation
-// import scala.util.Random
-// //import FUNCTION3._
-// class immetest extends FreeSpec with ChiselScalatestTester {
-//     "imme test" in {
-// test ( new ImmdValGen() ) { c =>
-// // br operations
-// val array_op = Array ( 0,1,4,5,6,7)
-// for ( i <- 0 until 100) {
-// // val src_a = Random . nextLong () & 0xFFFFFFFFL
-// // val src_b = Random . nextLong () & 0xFFFFFFFFL
-// val opr = Random . nextInt (6)
-// val type = array_op(opr)
-// //val branch1 = Random.nextBoolean()
-// // ALU functional implementation using Scala match
-// val result = type match {
-// case 0 => "b0010011"
-// case 1 => "b0100011"
-// case 4 => "b0110111"
-// case 5 => "b1100011"
-// case 6 => "b1101111"
-// case 7 => "b0000011"}
-// // c.io.arg_x.poke ( src_a .U )
-// // c.io.arg_y.poke ( src_b .U )
-// // c.io.branch.poke ( branch1 .B )
-// c.io.instr.poke ( type .U )
-// c.clock.step (1)
-// if (branch1 === 1.B){
-// c.io.br_taken.expect ( result.B )
-// } else if (branch1 === 0.B) {c.io.br_taken.expect (0.B)}
-// //c . clock . step (2)
-// }
-// }
-//     }}
+package lab4
+import chisel3._
+import chisel3.util._
+import org.scalatest._
+import chiseltest._
+import chiseltest.experimental.TestOptionBuilder._
+import chiseltest.internal.VerilatorBackendAnnotation
+import scala.util.Random
+
+class immediatetest extends FreeSpec with ChiselScalatestTester {
+    "imme test" in {
+test ( new ImmdValGen() ) { c =>
+val array_op = Array ( "b00000000011000110000001110010011","b00000000011000110000001110100011",
+"b00000000011000110000001110110111","b00000000011000110000001111100011",
+"b00000000011000110000001111101111")
+for ( i <- 0 until 100) {
+
+val opr1 = Random.nextInt (5)
+val type1_ = array_op(opr1)
+
+val result_ = type1_ match {
+case "b00000000011000110000001110010011" => 6
+case "b00000000011000110000001110100011" => 7
+case "b00000000011000110000001110110111" => 1584
+case "b00000000011000110000001111100011" => 1027
+case "b00000000011000110000001111101111" => 98307}
+
+c.io.instr.poke (type1_ .U )
+c.clock.step (1)
+c.io.immd_se.expect (result_ .U)
+
+}
+}
+}}
